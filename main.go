4a2b9a08e408fcd589a0361701706601186d858f
gc_luoxinghong <xinghong.luo@gientech.com>
2023-12-13 14:35:23 +0800

    测试合并lxh分支到dev

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
@@ -13,5 +14,6 @@ func main() {
 			return c.String(http.StatusOK, "Hello, World!")
 		},
 	)
+	fmt.Println("测试合并lxh分支到dev")
 	e.Logger.Fatal(e.Start(":6666"))
 }
